6bea7b6763ad1d580fc3d7258c309b0fcd4ec32c
Shreyansh <84006667+GuyInDoubt@users.noreply.github.com>
2021-06-10 20:58:00 +0530

    Fixes

--- app/Http/Controllers/imageController.php
+++ app/Http/Controllers/imageController.php
@@ -10,36 +10,37 @@ class imageController extends Controller
     public function index(Request $request){
         $file = $request->file("file");
         $name = $file->getClientOriginalName();
+        $path = 'storage/app/public/'. $name .'';
 
         $img=\Image::make($file);
-        $img->text('Rovae', 120, 100, function($font) {$font->file('Fonts/CVFont.ttf'); $font->size(90);});
-        $img->resize(700, 700, function ($constraint) {$constraint->aspectRatio(); $constraint->upsize();});
+        $img->text('Rovae', 120, 100, function($font) {$font->file(public_path('Fonts/CVFont.ttf')); $font->size(90);});
+        $img->resize(999, 999, function ($constraint) {$constraint->aspectRatio(); $constraint->upsize();});
         $img->save(storage_path('app/public/XXLarge-'. $name .''));
 
-        $img=\Image::make($file)->orientate();
-        $img->text('Rovae', 120, 100, function($font) {$font->file('Fonts/CVFont.ttf'); $font->size(90);});
-        $img->resize(600, 600, function ($constraint) {$constraint->aspectRatio(); $constraint->upsize();});
+        $img=\Image::make($file);
+        $img->text('Rovae', 120, 100, function($font) {$font->file(public_path('Fonts/CVFont.ttf')); $font->size(90);});
+        $img->resize(850, 850, function ($constraint) {$constraint->aspectRatio(); $constraint->upsize();});
         $img->save(storage_path('app/public/XLarge-'. $name .''));
 
-        $img=\Image::make($file)->orientate();
-        $img->text('Rovae', 120, 100, function($font) {$font->file('Fonts/CVFont.ttf'); $font->size(90);});
-        $img->resize(500, 500, function ($constraint) {$constraint->aspectRatio(); $constraint->upsize();});
+        $img=\Image::make($file);
+        $img->text('Rovae', 120, 100, function($font) {$font->file(public_path('Fonts/CVFont.ttf')); $font->size(90);});
+        $img->resize(700, 700, function ($constraint) {$constraint->aspectRatio(); $constraint->upsize();});
         $img->save(storage_path('app/public/Large-'. $name .''));
 
-        $img=\Image::make($file)->orientate();
-        $img->text('Rovae', 120, 100, function($font) {$font->file('Fonts/CVFont.ttf'); $font->size(90);});
-        $img->resize(400, 400, function ($constraint) {$constraint->aspectRatio(); $constraint->upsize();});
+        $img=\Image::make($file);
+        $img->text('Rovae', 120, 100, function($font) {$font->file(public_path('Fonts/CVFont.ttf')); $font->size(90);});
+        $img->resize(600, 600, function ($constraint) {$constraint->aspectRatio(); $constraint->upsize();});
         $img->save(storage_path('app/public/Medium-'. $name .''));
 
-        $img=\Image::make($file)->orientate();
-        $img->text('Rovae', 120, 100, function($font) {$font->file('Fonts/CVFont.ttf'); $font->size(90);});
+        $img=\Image::make($file);
+        $img->text('Rovae', 120, 100, function($font) {$font->file(public_path('Fonts/CVFont.ttf')); $font->size(90);});
         $img->save(storage_path('app/public/Original-'. $name .''));
         
         $imageDt = new imageDt();
         $imageDt->name = $name; 
-        $imageDt->path = 'storage/app/public/'. $name .'';
-        $imageDt->save();
+        $imageDt->path = $path;
+        $imageDt->save(); 
 
-        return back();
-    }    
+        return back();                                                                   
+    }  
 }
