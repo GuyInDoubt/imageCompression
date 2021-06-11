ce920d0e90a3be442950b6b1085a39276d8070b3
Shreyansh <84006667+GuyInDoubt@users.noreply.github.com>
2021-06-11 16:49:45 +0530

    Fixed compression, updated watermark

--- app/Http/Controllers/imageController.php
+++ app/Http/Controllers/imageController.php
@@ -11,29 +11,31 @@ class imageController extends Controller
         $file = $request->file("file");
         $name = $file->getClientOriginalName();
         $path = 'storage/app/public/'. $name .'';
+        $width =\Image::make($file)->width();   
+        $height =\Image::make($file)->height();
 
         $img=\Image::make($file);
-        $img->text('Rovae', 120, 100, function($font) {$font->file(public_path('Fonts/CVFont.ttf')); $font->size(90);});
-        $img->resize(999, 999, function ($constraint) {$constraint->aspectRatio(); $constraint->upsize();});
+        $img->insert('public/Frame-134.png', 'bottom-right', 10, 10);
+        $img->resize($width/1.93, $height/1.09, function ($constraint) {$constraint->aspectRatio(); $constraint->upsize();});
         $img->save(storage_path('app/public/XXLarge-'. $name .''));
 
         $img=\Image::make($file);
-        $img->text('Rovae', 120, 100, function($font) {$font->file(public_path('Fonts/CVFont.ttf')); $font->size(90);});
-        $img->resize(850, 850, function ($constraint) {$constraint->aspectRatio(); $constraint->upsize();});
+        $img->insert('public/Frame-134.png', 'bottom-right', 10, 10);
+        $img->resize($width/2.25, $height/1.28, function ($constraint) {$constraint->aspectRatio(); $constraint->upsize();});
         $img->save(storage_path('app/public/XLarge-'. $name .''));
 
         $img=\Image::make($file);
-        $img->text('Rovae', 120, 100, function($font) {$font->file(public_path('Fonts/CVFont.ttf')); $font->size(90);});
-        $img->resize(700, 700, function ($constraint) {$constraint->aspectRatio(); $constraint->upsize();});
+        $img->insert('public/Frame-134.png', 'bottom-right', 10, 10);
+        $img->resize($width/2.74, $height/1.54, function ($constraint) {$constraint->aspectRatio(); $constraint->upsize();});
         $img->save(storage_path('app/public/Large-'. $name .''));
 
         $img=\Image::make($file);
-        $img->text('Rovae', 120, 100, function($font) {$font->file(public_path('Fonts/CVFont.ttf')); $font->size(90);});
-        $img->resize(600, 600, function ($constraint) {$constraint->aspectRatio(); $constraint->upsize();});
+        $img->insert('public/Frame-134.png', 'bottom-right', 10, 10);
+        $img->resize($width/3.2, $height/1.8, function ($constraint) {$constraint->aspectRatio(); $constraint->upsize();});
         $img->save(storage_path('app/public/Medium-'. $name .''));
 
         $img=\Image::make($file);
-        $img->text('Rovae', 120, 100, function($font) {$font->file(public_path('Fonts/CVFont.ttf')); $font->size(90);});
+        $img->insert('public/Frame-134.png', 'bottom-right', 10, 10);
         $img->save(storage_path('app/public/Original-'. $name .''));
         
         $imageDt = new imageDt();
@@ -43,6 +45,7 @@ class imageController extends Controller
 
         return back();                                                                   
     }
+
     public function getLastImage(){
         $imageDt = imageDt::latest()->first();
         return response()->json($imageDt);
